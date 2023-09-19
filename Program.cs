7c597d9a088c408e01820fbe86097ec0187ecf65
Johnsonz4 <113399755+Johnsonz4@users.noreply.github.com>
2023-09-19 00:01:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿
+using sspLab2;
+/**
+*--------------------------------------------------------------------
+* File name: ssLab1
+* Project name: Lab 2 
+*--------------------------------------------------------------------
+* Author’s names and emails:  Zoe Johnson, johnsonz3@etsu.edu 
+* Course-Section: CSCI-2910-001
+* Creation Date: 9/18/23
+* -------------------------------------------------------------------
+*/
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace sslab2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+            List<VideoGame> videoGames = ReadVideoGameFromCSV();
+
+        // step 2 
+        Dictionary<string, List<VideoGame>> platformDictionary = videoGames
+            .GroupBy(game => game.Platform)
+            .ToDictionary(group => group.Key, group => group.ToList());
+
+        // step 3 
+        foreach (var platform in platformDictionary)
+        {
+            string platformName = platform.Key;
+            List<VideoGame> platformGames = platform.Value;
+
+           
+            var top5Games = platformGames
+                .OrderByDescending(game => game.GlobalSales)
+                .Take(5);
+
+            
+            Console.WriteLine($"Top 5 Games for Platform: {platformName}");
+            foreach (var game in top5Games)
+            {
+                Console.WriteLine(game.ToString());
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+
+   
+    static List<VideoGame> ReadVideoGameFromCSV()
+    {
+        List<VideoGame> videoGames = new List<VideoGame>();
+
+        
+        string filePath = @"/Users/zoe/Projects/videogames.csv";
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] values = lines[i].Split(',');
+
+                if (values.Length >= 10)
+                {
+                    VideoGame game = new VideoGame
+                    {
+                        Name = values[0],
+                        Platform = values[1],
+                        Year = int.Parse(values[2]),
+                        Genre = values[3],
+                        Publisher = values[4],
+                        NASales = double.Parse(values[5]),
+                        EUSales = double.Parse(values[6]),
+                        JPSales = double.Parse(values[7]),
+                        OtherSales = double.Parse(values[8]),
+                        GlobalSales = double.Parse(values[9])
+                    };
+
+                    videoGames.Add(game);
+                }
+                else
+                {
+                    Console.WriteLine($"Skipping line {i + 1}: Insufficient data.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        return videoGames;
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VideoGames.cs
@@ -0,0 +1,47 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: ssLab1
+* Project name: Lab 2 
+*--------------------------------------------------------------------
+* Author’s names and emails:  Zoe Johnson, johnsonz3@etsu.edu 
+* Course-Section: CSCI-2910-001
+* Creation Date: 9/18/23
+* -------------------------------------------------------------------
+*/
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Xml.Linq;
+
+
+namespace sspLab2
+{
+    class VideoGame : IComparable<VideoGame>
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NASales { get; set; }
+        public double EUSales { get; set; }
+        public double JPSales { get; set; }
+        public double OtherSales { get; set; }
+        public double GlobalSales { get; set; }
+
+
+
+        public int CompareTo(VideoGame other)
+        {
+
+            return string.Compare(this.Name, other.Name, StringComparison.Ordinal);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Title: {Name}, Platform: {Platform}, Year: {Year}, Genre: {Genre}, Publisher: {Publisher}, NASales: {NASales}, EUSales: {EUSales}, JPSales: {JPSales}, OtherSales: {OtherSales}, GlobalSales: {GlobalSales}";
+        }
+
+    }
+}
